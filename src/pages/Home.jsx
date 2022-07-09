ec4ce78a0aa030366291dceb9417bdda2149bc87
Jayanta Adhikary <73451350+jayantaadhikary@users.noreply.github.com>
2022-07-09 18:59:22 +0530

    added reducers

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UserResults from "../components/users/UserResults";
+
+function Home() {
+  return (
+    <>
+    {/* Search component */}
+      <UserResults />
+    </>
+  );
+}
+
+export default Home;
